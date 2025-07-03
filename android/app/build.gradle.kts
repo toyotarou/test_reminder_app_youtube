6d3172cf51e47ff1ecf13dcc9ca25dd8ddfa1571
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-07-03 10:42:40 +0900

    commit 20250703 初期準備

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,11 +8,13 @@ plugins {
 android {
     namespace = "com.example.test_reminder_app_youtube"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -28,6 +30,8 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -37,6 +41,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
